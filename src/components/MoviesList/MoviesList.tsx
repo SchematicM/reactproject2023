fe210f0737753536b8adb579f2ed78f78e1f46f4
mkhoma-appwell <mkhoma.appwell@ancestry.com>
2023-05-19 00:41:21 +0300

    fixed issue with page query in URL

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,34 +1,40 @@
 import React, {FC, useEffect} from 'react';
 
-import {useAppDispatch, useAppSelector} from "../../hooks";
+import {useAppDispatch, useAppLocation, useAppSelector} from "../../hooks";
 import {moviesActions} from "../../redux";
 import MovieListCard from "../MovieListCard/MovieListCard";
 
 import './moviesList.css'
-import {useSearchParams} from "react-router-dom";
+import {IQuery} from "../../interfaces";
 
-const MoviesList:FC = () => {
-    const{results} = useAppSelector(state => state.moviesReducer);
+
+const MoviesList: FC = () => {
+    const { results } = useAppSelector((state) => state.moviesReducer);
     const dispatch = useAppDispatch();
-    const [query, setQuery] = useSearchParams();
+    const location = useAppLocation<IQuery>();
 
     useEffect(() => {
-        setQuery(prev => ({...prev, page: '1'}))
-    }, [])
+        const searchParams = new URLSearchParams(location.search);
+        let page = searchParams.get('page');
+        if (!page) {
+            page = '1';
+            searchParams.set('page', page);
+            const newUrl = `${location.pathname}?${searchParams.toString()}`;
+            window.history.replaceState(null, '', newUrl);
+        }
+        dispatch(moviesActions.getAll(+page));
+    }, [dispatch, location.search]);
 
-    useEffect(() => {
-       // @ts-ignore
-        dispatch( moviesActions.getAll(+query.get('page')) );
-    }, [query])
     return (
-        <div >
+        <div>
             <div className={'movies-list'}>
-                {
-                    results.map(movie => <MovieListCard key={movie.id} movie = {movie}/> )
-                }
+                {results.map((movie) => (
+                    <MovieListCard key={movie.id} movie={movie} />
+                ))}
             </div>
         </div>
     );
 };
+
 
 export default MoviesList;
