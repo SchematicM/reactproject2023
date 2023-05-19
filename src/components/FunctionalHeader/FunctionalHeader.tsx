ccc80fa98d2b958a9bd3e7d5ebf98c1b1c5833c4
mkhoma-appwell <mkhoma.appwell@ancestry.com>
2023-05-19 20:03:48 +0300

    fixed search

--- src/components/FunctionalHeader/FunctionalHeader.tsx
+++ src/components/FunctionalHeader/FunctionalHeader.tsx
@@ -1,27 +1,21 @@
 import React, {FC} from 'react';
 import './functionalHeader.css';
 import {SubmitHandler, useForm} from "react-hook-form";
-import { useAppLocation} from "../../hooks";
 import {IQuery} from "../../interfaces";
 
 
 const FunctionalHeader: FC = () => {
     const {handleSubmit, register} = useForm<IQuery>();
-    const location = useAppLocation<IQuery>();
 
     const search: SubmitHandler<IQuery> = async (data) => {
-        const searchParams = new URLSearchParams(location.search);
-        searchParams.set('query', data.query);
-        const page:string = '1';
-        searchParams.set('page', page);
-        const newUrl = `${location.pathname}?page=${page}&query=${encodeURIComponent(searchParams.get('query')!)}`;
-        window.history.replaceState(null, '', newUrl);
+        const query = `page=1&query=${data.query}`;
+        window.location.href = `/movies?${query}`;
     };
 
     return (
             <form onSubmit={handleSubmit(search)} className={'search'}>
                 <input type="text" placeholder="Search" {...register('query', {required: true})} />
-                <button type="submit">Search</button>
+                <button type="submit"><i className="fa fa-search" aria-hidden="true"></i></button>
             </form>
     );
 };
--- src/components/MoviesPagination/MoviesPagination.tsx
+++ src/components/MoviesPagination/MoviesPagination.tsx
@@ -6,19 +6,20 @@ import './moviesPagination.css';
 const MoviesPagination:FC = () => {
     const {page,total_pages} = useAppSelector(state => state.moviesReducer);
     const [query,setQuery] = useSearchParams();
-    console.log(query.get('page'));
     const currentPage = Number(query.get('page')) || 1;
 
     const prev = () => {
         setQuery((prevQuery) => {
             const newQuery = new URLSearchParams(prevQuery);
             newQuery.set('page', String(currentPage - 1));
+            window.scrollTo(0, 0);
             return newQuery;
         })    }
     const next = ()=>{
         setQuery((prevQuery) => {
             const newQuery = new URLSearchParams(prevQuery);
             newQuery.set('page', String(currentPage + 1));
+            window.scrollTo(0, 0);
             return newQuery;
         })
     }
