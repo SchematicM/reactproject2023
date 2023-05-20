aff09a1f9231dc8ffecd52f8353f81caaf071339
Marta Khoma <tyxa.khoma16@gmail.com>
2023-05-20 16:05:54 +0300

    Added half-stars rating displaying functionality

--- src/components/StarsRating/StarsRating.tsx
+++ src/components/StarsRating/StarsRating.tsx
@@ -10,23 +10,24 @@ const StarsRating: React.FC<StarsRatingProps> = ({rating}) => {
     const totalStars = 10;
     const newRating = rating.toFixed(1);
 
+    function getStarClass(rating: number, starValue: number): [string, string] {
+        const starFillTypeClass =  (rating - starValue) < 1 && ((rating - starValue) % 1) >= 0.4 ? "half" : "filled";
+        const starColorRatingClass = rating >= 8 ? "green" : rating >= 6 ? "orange" : "red";
+
+        return [starFillTypeClass, starColorRatingClass];
+    }
+
     return (
         <div className="rating">
             <div className={'rating-number'}>{newRating}</div>
             {[...Array(totalStars)].map((_, index) => {
                 const starValue = index + 1;
-                const filledStar = starValue <= rating || starValue - 0.5 === rating;
-                const colorRatingClass = rating >= 8 ? "green" : rating >= 6 ? "orange" : "red"
-                const starClassName = filledStar ? `star filled ${colorRatingClass}` : 'star';
-                const starIcon = filledStar ? '★' : '☆';
+                const ifFilledStar = starValue <= rating;
+                const [starFillTypeClass, starColorRatingClass] = getStarClass(rating, starValue);
+                const starClassName = ifFilledStar ? `star ${starFillTypeClass} ${starColorRatingClass}` : 'star';
 
                 return (
-                    <span
-                        key={index}
-                        className={starClassName}
-                    >
-            {starIcon}
-          </span>
+                    <span key={index} className={starClassName}></span>
                 );
             })}
         </div>
